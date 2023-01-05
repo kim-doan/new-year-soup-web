b1062dbb32f7cf8126637b071041ab34338b00f8
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-05 16:58:46 +0900

    feat: 로그인페이지 임의 생성

---
+++ app/auth/login/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <title>로그인 페이지</title>
+    </>
+  );
+}
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import React from 'react';
+import { useForm, Resolver } from 'react-hook-form';
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+};
+
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.firstName ? values : {},
+    errors: !values.firstName
+      ? {
+          firstName: {
+            type: 'required',
+            message: 'This is required.',
+          },
+        }
+      : {},
+  };
+};
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver });
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input {...register('firstName')} placeholder="Bill" />
+      {errors?.firstName && <p>{errors.firstName.message}</p>}
+
+      <input {...register('lastName')} placeholder="Luo" />
+
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Login;
