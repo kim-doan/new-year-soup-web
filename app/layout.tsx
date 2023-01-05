8c7883eb2c94a21224b2f87f2bd4ac99bdeada3f
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-05 16:58:11 +0900

    feat: 레이아웃 템플릿 추가 및 스타일 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import './globals.css'
+import './globals.css';
+import Template from './template';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html>
       <head>
         <title>메인페이지</title>
-        </head>
-      <body>{children}</body>
+      </head>
+      <body>
+        <Template>{children}</Template>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,3 @@
-import { Inter } from '@next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-const Home = () => (
-  <div>test</div>
-  // <Wrapper>
-  //   <Title>홈페이지</Title>
-  // </Wrapper>
-);
+const Home = () => <div>홈페이지</div>;
 
 export default Home;
-
-// const Wrapper = styled.div`
-//   background: #efefef;
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// const Title = styled.div`
-//   font-size: 1.2rem;
-//   color: #424242;
-// `;
