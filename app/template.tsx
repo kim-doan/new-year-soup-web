15605cbfcd8a2502424ca3782aa0d1ddfc66888a
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-05 16:58:20 +0900

    feat: 레이아웃 템플릿 추가 및 스타일 적용

--- app/template.tsx
+++ app/template.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import RootWrapper from './RootWrapper';
+import styles from './template.module.css';
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <main className={styles.main}>
       <RootWrapper>{children}</RootWrapper>
-    </>
+    </main>
   );
 }
