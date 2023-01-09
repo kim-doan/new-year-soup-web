bb8a5a1f00a5fb1f7a0c568fced460ab0ae00734
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-09 14:36:00 +0900

    feat: 회원가입 아이디 중복 오류 추가

--- app/auth/register/services/registerServices.ts
+++ app/auth/register/services/registerServices.ts
@@ -1,13 +1,19 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, type AuthError } from 'firebase/auth';
 import { authService } from '../../../config/firebase';
 
 export const createUser = async (id: string, pw: string) => {
-  const convertEmail = id + '@newyearsoup.com';
-  const res = await createUserWithEmailAndPassword(
-    authService,
-    convertEmail,
-    pw
-  );
+  try {
+    const convertEmail = id + '@newyearsoup.com';
+    const res = await createUserWithEmailAndPassword(
+      authService,
+      convertEmail,
+      pw
+    );
 
-  return res;
+    return res;
+  } catch (e) {
+    if (String(e).includes('auth/email-already-in-use')) {
+      alert('이미 사용중인 아이디 입니다');
+    }
+  }
 };
