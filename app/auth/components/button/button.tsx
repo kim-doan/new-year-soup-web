e811c1f48688fbcebe05bd3e65fe4d867ac14537
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-10 17:50:10 +0900

    feat: 공용 버튼 컴포넌트 구현

---
+++ app/auth/components/button/button.tsx
@@ -0,0 +1,34 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import styles from './button.module.css';
+
+type ButtonStatus = 'main' | 'primary';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  status: ButtonStatus;
+  onClick?: () => void;
+}
+
+const Button = ({ status, children, type, onClick }: ButtonProps) => {
+  const matchStatus = () => {
+    switch (status) {
+      case 'main':
+        return styles.main;
+      case 'primary':
+        return styles.primary;
+      default:
+        return styles.main;
+    }
+  };
+
+  return (
+    <button
+      className={`${styles.btn} ${matchStatus()}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
