6680f80ed0daacdeb221ebe512f6f7e1f89a3e2d
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-10 16:38:17 +0900

    feat: gnb 임의 구현

---
+++ app/auth/components/layout/navigationBar.tsx
@@ -0,0 +1,11 @@
+import styles from './navigationBar.module.css';
+
+const NavigationBar = () => {
+  return (
+    <div className={styles.navigationBar}>
+      <button>=</button>
+    </div>
+  );
+};
+
+export default NavigationBar;
