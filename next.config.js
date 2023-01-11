c19d79473683f5713f41b7eeb64689b6041acaf5
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-11 14:46:02 +0900

    feat: 타입스크립트 baseUrl 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
