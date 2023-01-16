6766b9556a59209029b5c4299e22d73f17317916
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-16 16:17:40 +0900

    feat: 메인페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 import { useRecoilState } from 'recoil';
+import Button from './components/button/button';
 import { authState } from './core/states/authState';
+import styles from './page.module.css';
 
 const Home = () => {
   const [user] = useRecoilState(authState);
 
   return (
-    <div>
-      <h1>홈페이지</h1>
-      {user && <p>{user}님 어서오세요</p>}
-    </div>
+    <section className={styles.page}>
+      <div className={styles.background}></div>
+      <div className={styles.buttonWrapper}>
+        <Button status="main">시작하기</Button>
+      </div>
+    </section>
   );
 };
 
