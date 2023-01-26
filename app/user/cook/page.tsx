39073582b8a0d2fac2afcebae18465a07a5a4408
이준성 <junsung.lee@MB-junsunglee.local>
2023-01-26 17:10:57 +0900

    feat: 떡국 페이지 배경 제작

--- app/user/cook/page.tsx
+++ app/user/cook/page.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
+import { soupBowl1, soupBowl2 } from '../assets/soups';
+
 import SlideMenu from './components/slideMenu';
 import styles from './cookPage.module.css';
 
 const CookPage = () => {
   return (
     <section className={styles.page}>
-      <div>a</div>
+      <div className={styles.soupTray}>
+        <Image src={soupBowl2} alt="a" width={240} />
+      </div>
       <SlideMenu />
     </section>
   );
