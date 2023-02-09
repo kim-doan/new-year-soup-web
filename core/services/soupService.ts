ae8e3457ef3674a8ea7920e367c086407e31cb14
이준성 <junsung.lee@MB-junsunglee.local>
2023-02-09 15:57:47 +0900

    feat: getSoupDetail api 추가

--- core/services/soupService.ts
+++ core/services/soupService.ts
@@ -1,4 +1,6 @@
 import { SoupApi } from 'apiClients/soupApi';
+import axios from 'axios';
+import { fbAuth } from 'common/lib/firebase/firebase';
 
 export default class SoupService {
   private soupApi: SoupApi;
@@ -17,21 +19,43 @@ export default class SoupService {
     }
   };
 
+  public getSoupDetail = async (soupNo: number) => {
+    try {
+      const idToken = await fbAuth.currentUser?.getIdToken();
+      axios.defaults.headers.common['Authorization'] = `Bearer ${idToken}`;
+
+      if (idToken) {
+        const res = await this.soupApi.getSoupDetail(soupNo);
+
+        return res.data;
+      }
+    } catch (e) {
+      alert('떡국의 메시지를 불러오는데 실패했습니다.');
+    }
+  };
+
   public deliverySoup = async (
-    resUserId: string,
+    reqUserId: string,
     soupContents: string,
     soupImgId: string
   ) => {
     try {
-      const res = await this.soupApi.deliverySoup({
-        resUserId,
-        soupContents,
-        soupImgId,
-      });
+      const idToken = await fbAuth.currentUser?.getIdToken();
+      if (idToken) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${idToken}`;
+        await this.soupApi.deliverySoup({
+          reqUserId,
+          soupContents,
+          soupImgId,
+        });
 
-      return res.data;
+        return true;
+      } else {
+        alert('로그인 후 이용이 가능합니다');
+      }
     } catch (e) {
       alert('떡국을 전달하는데 실패했습니다');
+      return false;
     }
   };
 }
